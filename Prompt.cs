7ffca5c7ed9165936858cfe40d46acd7d8f3faaa
Michael Meyer <michael.meyer@improving.com>
2021-06-30 10:28:41 -0500

    initial commit

---
+++ Prompt.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MadLibs
+{
+    class Prompt
+    {
+        public string Story { get; set; }
+        public List<KeyValuePair<string, string>> Blanks { get; set; }
+    }
+}
