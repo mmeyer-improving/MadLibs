8ddb6fcde43b561fcf8ae562e7110a8d33a0d567
Michael Meyer <michael.meyer@improving.com>
2021-06-30 12:50:27 -0500

    initial implementation

--- Program.cs
+++ Program.cs
@@ -6,7 +6,16 @@ namespace MadLibs
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string anotherPrompt;
+            do
+            {
+                Prompt prompt = new Prompt();
+                prompt.FillInBlanks();
+                prompt.DisplayMadLib();
+                Console.WriteLine("Do you want another Mad Lib? Type 'y' for one more, or any other key to quit ");
+                anotherPrompt = Console.ReadLine();
+            } while (anotherPrompt == "y");
+            Console.WriteLine("Goodbye.");
         }
     }
 }
--- Prompt.cs
+++ Prompt.cs
@@ -9,6 +9,34 @@ namespace MadLibs
     class Prompt
     {
         public string Story { get; set; }
-        public List<KeyValuePair<string, string>> Blanks { get; set; }
+        public List<string> Blanks { get; set; }
+        public List<string> Answers { get; set; }
+
+        public Prompt ()
+        {
+            //TODO: Refactor to initialize prompt with values pulled from ADO.NET
+            this.Story = "My friend's name is {0}. They like {1}. They are {2}.";
+            this.Blanks = new List<string>() { 
+                "Friend's Name",
+                "Noun",
+                "Adjective"
+            };
+            this.Answers = new List<string>();
+        }
+
+        public void FillInBlanks()
+        {
+            foreach (var blank in Blanks)
+            {
+                Console.Write($"Give me a {blank}: ");
+                string answer = Console.ReadLine();
+                Answers.Add(answer);
+            }
+        }
+
+        public void DisplayMadLib()
+        {
+            Console.WriteLine(String.Format(Story, Answers.ToArray()));
+        }
     }
 }
