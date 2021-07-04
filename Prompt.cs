4b7a9408e6591c846e31eecea34a7e9b7bc4a574
Michael Meyer <michael.meyer@improving.com>
2021-07-03 19:58:10 -0500

    currently blocked but uploading progress

--- Prompt.cs
+++ Prompt.cs
@@ -12,18 +12,6 @@ namespace MadLibs
         public List<string> Blanks { get; set; }
         public List<string> Answers { get; set; }
 
-        public Prompt ()
-        {
-            //TODO: Refactor to initialize prompt with values pulled from ADO.NET
-            this.Story = "My friend's name is {0}. They like {1}. They are {2}.";
-            this.Blanks = new List<string>() { 
-                "Friend's Name",
-                "Noun",
-                "Adjective"
-            };
-            this.Answers = new List<string>();
-        }
-
         public void FillInBlanks()
         {
             foreach (var blank in Blanks)
---
+++ PromptData.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MadLibs
+{
+    class PromptData
+    {
+        public Prompt GetPrompt()
+        {
+            var prompt = new Prompt();
+
+            using (var conn = new SqlConnection("Server=.;Database=MichaelMadLibs;Trusted_Connection=True;"))
+            {
+                conn.Open();
+
+                var cmd = new SqlCommand();
+                cmd.Connection = conn;
+                cmd.CommandType = System.Data.CommandType.Text;
+
+                cmd.CommandText = "SELECT COUNT(*) FROM Prompts";
+                int numRows = cmd.ExecuteReader();
+
+                
+            }
+        }
+    }
+}
